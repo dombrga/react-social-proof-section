8113d3d109a0825571508f73af5b479007e281df
dom <dom.barga@gmail.com>
2022-12-20 16:50:17 +0800

    started overview section

--- src/App.tsx
+++ src/App.tsx
@@ -25,15 +25,20 @@ function App() {
     } catch (err) {
       console.error('Error:', err)
     }
-    
+
   }, [])
 
 
   return (
     <div className={styles.app}>
       <main>
-        <div className=''>
-
+        <div className={styles.overview}>
+          <h1>
+            10,000+ of our users love our products.
+          </h1>
+          <p>
+            We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
+          </p>
         </div>
         <div className={styles.ratings}>
           {ratings.map((rating: Rating) => {

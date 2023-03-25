de071ce20bcb48d2dc205f69b9e14d65a6a66e41
dombrga <dominic.o.barga@accenture.com>
2023-03-26 01:40:33 +0800

    styled reviews card

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -14,10 +14,10 @@ function ReviewCard({ review }: props) {
   return (
     <article className={style.review}>
       <div className={style.review__reviewer}>
-        <img src={image} alt="" className="avatar" />
-        <div>
-          <p>{ review.name }</p>
-          { review.isVerifiedBuyer && <p>Verified Buyer</p> }
+        <img src={image} alt="" className={style.review__reviewer_avatar} />
+        <div className={style.review__reviewer_info}>
+          <p className={style.review__reviewer_name}>{ review.name }</p>
+          { review.isVerifiedBuyer && <p className={style.review__reviewer_verified}>Verified Buyer</p> }
         </div>
       </div>
       <p className='review__content'>
